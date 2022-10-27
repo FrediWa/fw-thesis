70845f631c4e6bb4828a9e05626f27c066a05559
Fredrik Wasström <fredrik.wasstrom@korulab.com>
2022-10-27 14:44:10 +0300

    save

--- scripts/context.js
+++ scripts/context.js
@@ -7,3 +7,16 @@ const ApplicationContext = {
         playbackArray: []
 }
 
+// Static constants
+
+// https://mosaicmusicinstruction.com/wp-content/uploads/2018/07/basic-tempo-markings.pdf
+const TEMPO = {
+    LARGO: 50,
+    LENTO: 55,
+    ADAGIO: 70,
+    MODERATO: 110,
+    ALLEGRETTO: 120,
+    ALLEGRO: 145,
+    PRESTO: 180,
+}
+
